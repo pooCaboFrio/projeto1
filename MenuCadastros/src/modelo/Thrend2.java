7742d4f7e5970cb468386a7afff810369ab9cb3d
pooCaboFrio <poocabofrio@yahoo.com.br>
2013-10-25 22:14:52 -0200

    Create Thrend2.java

---
+++ MenuCadastros/src/modelo/Thrend2.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trabalho;
+
+/**
+ *25out2013
+ * Rodolfo, Mateus, Daniel
+ */
+public class Thread2 extends Thread{
+    int i =2;
+    public void run(){
+        while (i>0){
+            System.out.println(Thread.currentThread().getName());
+        }
+    }
+}
